2e12055bde69ec6c840317e4aa5ee8b5d0bc0c5b
Felipe Caue <felipecaue.fraga@gmail.com>
2015-05-30 18:01:01 -0300

    remove de index code  of mongo db
    remove de index code  of mongo db and add in file createIndex.js

--- geocoder.js
+++ geocoder.js
@@ -16,17 +16,6 @@ rl.question("Qual é o endereço:", function(enderecos){
 var MongoClient = require('mongodb').MongoClient
     ,url = 'mongodb://localhost:27017/geodb';
 
-MongoClient.connect(url, function(err, db) {
-  if(err) return console.dir(err)
-
-  var collection = db.collection('enderecos');
-
-  collection.ensureIndex({longitude: "2d"}, {min: -1000, max: 1000}, function(err, result) {
-    if(err) return console.dir(err);
-    return console.log(result);
-  });
-
-});
 
 MongoClient.connect(url, function(err, db) {
    if(err) return console.dir(err)
