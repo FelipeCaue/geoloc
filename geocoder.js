9ef70438e29dae456c7c1a0d20b55a5af73c535d
Felipe Caue <felipecaue.fraga@gmail.com>
2015-05-30 16:14:26 -0300

    Identando o codigo

--- geocoder.js
+++ geocoder.js
@@ -7,22 +7,9 @@ var rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("Qual o endereco:", function(args){
+rl.question("Qual é o endereço:", function(args){
   var geocoder = require('node-geocoder')(geocoderProvider, httpsAdapter);
-
-
-geocoder.geocode({address: args, country: 'Brazil'}, function(err, res) {
+  geocoder.geocode({address: args, country: 'Brazil'}, function(err, res) {
     console.log(res);
-});
-
+  });
 })
-//var readline = require('')
-//var endereco;
-/*
-// optionnal
-var extra = {
-    apiKey: 'YOUR_API_KEY', // for Mapquest, OpenCage, Google Premier
-    formatter: null         // 'gpx', 'string', ...
-};
-*/
-
