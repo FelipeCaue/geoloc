174eb6e280f5a4b82298dc607231366f005dccfd
Felipe Caue <felipecaue.fraga@gmail.com>
2015-06-04 13:12:00 -0300

    remove connection with mongoldb

--- geocoder.js
+++ geocoder.js
@@ -11,20 +11,22 @@ var rl = readline.createInterface({
 rl.question("Qual é o endereço:", function(enderecos){
   var geocoder = require('node-geocoder')(geocoderProvider, httpsAdapter);
   geocoder.geocode({address: enderecos, country: 'Brazil'}, function(err, res) {
-    //console.log(res);
+    console.log(res);
+    var endereco = res;
+    console.log("Variavel End:" + endereco);
+// var MongoClient = require('mongodb').MongoClient
+//     ,url = 'mongodb://localhost:27017/geodb';
 
-var MongoClient = require('mongodb').MongoClient
-    ,url = 'mongodb://localhost:27017/geodb';
 
+// MongoClient.connect(url, function(err, db) {
+//    if(err) return console.dir(err)
 
-MongoClient.connect(url, function(err, db) {
-   if(err) return console.dir(err)
+//   db.collection('enderecos').insert(res, function(err, result) {
+//     if(err) return console.dir(err)
+//       return console.log('Lugar cadastrado: ', res.longitude);
+//      });
 
-  db.collection('enderecos').insert(res, function(err, result) {
-    if(err) return console.dir(err)
-      return console.log('Lugar cadastrado: ', res);
-     });
-
-    });
-  })
+//     });
+   })
+  rl.close();
 });
